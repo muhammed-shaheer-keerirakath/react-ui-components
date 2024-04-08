1034b2f79971bee6ff8f3b46d88baf838403ee78
Shaheer Shukur <shaheershukur@gmail.com>
2024-04-09 02:34:19 +0400

    feat: updates vite lib config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "src/lib/index"),
+      formats: ["es"],
+    },
+  },
+  plugins: [
+    react(),
+    dts({ include: ["src/lib"], exclude: ["src/**/*.spec.tsx"] }),
+  ],
 });
